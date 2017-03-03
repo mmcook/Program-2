3f980cb8b2e08b62b9d41ef48461d0c3286f9483
ckairis <kairis@wisc.edu>
2017-03-02 18:54:49 -0600

    Update JobList.java

--- JobList.java
+++ JobList.java
@@ -1,130 +1,177 @@
 import java.util.Iterator;
 
 public class JobList implements ListADT<Job> {
+
+	//data members
+	private int numJobs;
 	
-	private Iterator<Job> iterator;
-	private Listnode<Job> head, curr;
-	private int numItems;
+	//private Iterator<Job> itr;
 	
+	//Reference to the header node in a JobList instance
+	private Listnode<Job> head;
+	
+	/** This is the constructor for each JobList instance. Specifies the number
+	 * of Jobs in the list and the reference to the list itself
+	 * 
+	 * @param none
+	 */
 	public JobList() {
-		head = new Listnode<Job>(null);
-		curr = head;
-		numItems = 0;
+		this.numJobs = 0;
+		this.head = new Listnode<Job>(null);
 	}
-
-	@Override
+	
 	public Iterator<Job> iterator() {
-		iterator = new JobListIterator(head); 
-		return iterator;
+		
+		Iterator<Job> itr =  new JobListIterator(head.getNext());
+		return itr;
 	}
-
-	@Override
-	public void add(Job item) throws IllegalArgumentException {
-		if (item == null) {
-			throw new IllegalArgumentException();
-		}
+	
+	/** This method adds a Job to the end of the JobList
+	 * 
+	 * @param the job to be added
+	 * @return none
+	 * @throws IllegalArgumentException if job input is null
+	 */
+	public void add(Job e) {
+		if (e == null) throw new IllegalArgumentException();
 		
+		Listnode<Job> curr = head;
 		
-		while (curr.getNext() != null) {
+		while(curr.getNext() != null) {
 			curr = curr.getNext();
 		}
 		
-		Listnode<Job> e = new Listnode<Job>(item);
-		curr.setNext(e);
-		
-		
-		numItems++;
+		curr.setNext(new Listnode<Job>(e));
+		numJobs++;
 	}
-
-	@Override
-	public void add(int pos, Job item) throws IndexOutOfBoundsException, IllegalArgumentException {
-		if (pos < 0 || pos > numItems - 1) {
-			throw new IndexOutOfBoundsException();
-		}
-		
-		if (item == null) {
+	
+	/** This method adds a job at a designated position within the JobList
+	 * 
+	 * @param position to add the job
+	 * @param Job to be added
+	 * @return none
+	 * @throws IndexOutOfBoundsException if position is faulty
+	 * @throws IllegalArgumentException if job input is null
+	 * 
+	 */
+	public void add(int pos, Job e) {
+		if(e == null){
 			throw new IllegalArgumentException();
 		}
 		
-		if(pos == numItems) {
-			add(item);
+		if(pos < 0 || pos > numJobs) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		// if asked to add item to the end of the List
+		if(pos == numJobs) {
+			add(e);
 			return;
 		}
 		
-		curr = head.getNext();
-		
-		for (int i = 0; i < pos - 1; i++) {
+		Listnode<Job> curr = head;
+		for(int i = 0; i < pos; i++) {
 			curr = curr.getNext();
 		}
+		curr.setNext(new Listnode<Job>(e, curr.getNext()));
 		
-		curr.setNext(new Listnode<Job>(item, curr.getNext()));
-		
-		numItems++;
+		numJobs++;
 	}
-
-	@Override
-	public boolean contains(Job item) {
-		curr = head.getNext();
+	
+	/** This method checks if a JobList contains a particular job
+	 * 
+	 * @param the job being checked for in the JobList
+	 * @return whether or not the JobList contains that Job
+	 * @throws IllegalArgumentException if job input is null
+	 */
+	public boolean contains(Job e) {
+		if (e == null) {
+			throw new IllegalArgumentException();
+		}
+		Listnode<Job> curr = head;
 		
-		while (curr.getData() != null) {
-			if (curr.getData().equals(item)) {
+		for (int i = 0; i < numJobs; i++) {
+			if (curr.getNext().getData() == e) {
 				return true;
 			}
-			else {
-				curr = curr.getNext();
-			}
 		}
 		return false;
 	}
-
-	@Override
-	public int size() {
-		return numItems;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		if (numItems == 0) {
-			return true;
-		}
-		return false;
-	}
-
-	@Override
-	public Job get(int pos) throws IndexOutOfBoundsException {
+	
+	/** Returns the job at a specified index in the JobList
+	 * 
+	 * @param position of job that is desired
+	 * @return the job at the specified position
+	 * @throws IndexOutOfBoundsException when position is faulty
+	 */
+	public Job get(int pos) {
+		//Check if pos is a good input or not, if not then throw
+		//Exception
+		if (pos < 0 || pos > numJobs) throw new IndexOutOfBoundsException();
 		
-		if (pos < 0 || pos > numItems - 1) {
-			throw new IndexOutOfBoundsException();
-		}
+		//Create duplicate reference
+		Listnode<Job> curr = head;
 		
-		curr = head;
-		
+		//Traverse to the position of interest
 		for (int i = 0; i < pos; i++) {
 			curr = curr.getNext();
 		}
 		
-		return curr.getData();
+		//Return Job that curr's Listnode references
+		return curr.getNext().getData();
 	}
-
-	@Override
-	public Job remove(int pos) throws IndexOutOfBoundsException {
+	
+	/** This method returns a boolean value indicating whether the JobList
+	 *  is empty or not
+	 *  
+	 *  @param none
+	 *  @return if number of Jobs in JobList is zero or not
+	 * 
+	 */
+	public boolean isEmpty() {
+		return numJobs == 0;
+	}
+	
+	/** Removes a Job from the JobList at a specified position
+	 * 
+	 * @param position where removal is desired
+	 * @return Job at that position
+	 * @throws IndexOutOfBoundsException when position is faulty
+	 */
+	public Job remove(int pos) {
 		
-		if (pos < 0 || pos > numItems - 1) {
+		if (pos < 0 || pos > numJobs) {
 			throw new IndexOutOfBoundsException();
 		}
 		
-		curr = head.getNext();
+		//Initialize two place holder Listnodes
+		Listnode<Job> curr = head;
+		Listnode<Job> temp;
 		
-		for (int i = 0; i < pos - 1; i++) {
+		//Traverse to the node right before the deisred node 
+		for (int i = 0; i < pos; i++) {
 			curr = curr.getNext();
 		}
 		
+		temp = curr.getNext();
+		
+		//Set previous node's next reference to the node after the node to 
+		//be removed.
 		curr.setNext(curr.getNext().getNext());
 		
-		numItems--;
+		numJobs--;
 		
-		return curr.getNext().getData();
+		//Return the data within the 
+		return temp.getData();
 	}
-
+	
+	/** Returns the number of Jobs in the JobList
+	 * 
+	 * @param none
+	 * @return number of jobs in the JobList
+	 */
+	public int size() {
+		return numJobs;
+	}
 	
 }
